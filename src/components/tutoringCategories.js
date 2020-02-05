952ba6d7a0c6c513f025d17ddd41b2fe6fc27c47
Nick Land <nicklandiam@gmail.com>
2020-02-04 17:36:24 -0700

    category page, very rough

--- src/components/tutoringCategories.js
+++ src/components/tutoringCategories.js
@@ -1,10 +1,353 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class TutoringCategories extends Component {
   render() {
     return (
       <div className="container">
-        <div>Hello</div>
+        <div className="categoryLead">
+          <h1>Tutoring Categories</h1>
+        </div>
+        <div className="row row-cols-4">
+          <div className="col">
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>{" "}
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+        <div className="row row-cols-4">
+          <div className="col">
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>{" "}
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+        <div className="row row-cols-4">
+          <div className="col">
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>{" "}
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+        <div className="row row-cols-4">
+          <div className="col">
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>{" "}
+          </div>
+          <div className="col">
+            {" "}
+            <div className="card">
+              <div className="card-header">Writing</div>
+              <div className="card-body">
+                <span>
+                  <h5>
+                    Queue : <b>12</b>
+                  </h5>
+                </span>
+                <span>
+                  <h6>
+                    ETA : <b>30min</b>
+                  </h6>
+                </span>
+                <Link to="/validate">
+                  <button className="btn btn-dark ">Waitlist</button>
+                </Link>
+              </div>
+            </div>{" "}
+          </div>
+        </div>
       </div>
     );
   }
