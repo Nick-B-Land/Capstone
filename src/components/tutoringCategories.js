cb8576cbeabbca117a1b1b240f0d165ada22ab40
Nick Land <nicklandiam@gmail.com>
2020-02-10 18:21:56 -0700

    brainfart moment, all is good

--- src/components/tutoringCategories.js
+++ src/components/tutoringCategories.js
@@ -1,39 +1,42 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import CategoryRender from "./categoryRender";
+import { observer } from "mobx-react";
 
-class TutoringCategories extends Component {
-  componentDidMount = () => {
-    //console.log(this.props.catStore.Categories[0].doc._id);
-    //console.log(this.props.catStore.Categories);
-    console.log(this.props.catStore.Fuckyourshit);
-  };
+const tutoringCategories = observer(
+  class TutoringCategories extends Component {
+    componentDidMount = () => {
+      //console.log(this.props.catStore.Categories[0].doc._id);
+      //console.log(this.props.catStore.Categories);
+      console.log(this.props.catStore.Fuckyourshit);
+    };
 
-  renderCategories = () => {
-    let Cats = this.props.catStore.Categories;
+    renderCategories = () => {
+      let Cats = this.props.catStore.Categories;
 
-    // Cats.forEach(e=> {
-    //   return
-    // })
+      // Cats.forEach(e=> {
+      //   return
+      // })
 
-    return Cats.map(e => (
-      <section key={e.doc._id}>
-        {console.log(e.doc._id)}
-        <CategoryRender currentQ={e.doc.CurrentQ} ETA={e.doc.ETA} />
-      </section>
-    ));
-  };
+      return Cats.map(e => (
+        <section key={e.doc._id}>
+          {console.log(e.doc._id)}
+          <CategoryRender currentQ={e.doc.CurrentQ} ETA={e.doc.ETA} />
+        </section>
+      ));
+    };
 
-  render() {
-    return (
-      <div className="container">
-        <div className="categoryLead">
-          <h1>Tutoring Categories</h1>
+    render() {
+      return (
+        <div className="container">
+          <div className="categoryLead">
+            <h1>Tutoring Categories</h1>
+          </div>
+          {this.renderCategories()}
         </div>
-        {this.renderCategories()}
-      </div>
-    );
+      );
+    }
   }
-}
+);
 
-export default TutoringCategories;
+export default tutoringCategories;
