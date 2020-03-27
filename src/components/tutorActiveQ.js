ffe60e9a6446baa71ac248951f9a0865c253de90
Nick Land <nicklandiam@gmail.com>
2020-03-26 19:16:36 -0600

    sample styling of page spacing

--- src/components/tutorActiveQ.js
+++ src/components/tutorActiveQ.js
@@ -37,29 +37,45 @@ class TutorActiveQ extends Component {
 
   render() {
     return (
-      <div className="container tutorActiveDiv">
+      <div className="container">
         <div className="row">
-          <div className="col-4 text-center">
-            <h2>00:00</h2>
+          <div className="col-4"></div>
+          <div className="col-8">
+            <div className="row d-flex justify-content-end">
+              <div className="tutorActiveDiv">
+                <div className="row">
+                  <div className="col-4 text-center">
+                    <h2>00:00</h2>
+                  </div>
+                  <div className="col-8 text-center">
+                    {" "}
+                    <h2>Student Name - Student ID</h2>{" "}
+                  </div>
+                </div>
+                <div className="row d-flex justify-content-center">
+                  <h3>{this.setAppointmentStatus()}</h3>
+                </div>
+                <div className="row d-flex justify-content-between">
+                  {this.state.appointmentState === "readyToStart" ? (
+                    <button className="btn btn-lg qBtn lBtn">
+                      {" "}
+                      Start Appointment
+                    </button>
+                  ) : (
+                    <button className="btn btn-lg  qBtn lBtn">
+                      {" "}
+                      End Appointment
+                    </button>
+                  )}
+                  <button className="btn btn-lg qBtn rBtn d-flex flex-row-reverse">
+                    {" "}
+                    No Show
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="row"></div>
           </div>
-          <div className="col-8 text-center">
-            {" "}
-            <h2>Student Name - Student ID</h2>{" "}
-          </div>
-        </div>
-        <div className="row d-flex justify-content-center">
-          <h3>{this.setAppointmentStatus()}</h3>
-        </div>
-        <div className="row d-flex justify-content-between">
-          {this.state.appointmentState === "readyToStart" ? (
-            <button className="btn btn-lg qBtn lBtn"> Start Appointment</button>
-          ) : (
-            <button className="btn btn-lg  qBtn lBtn"> End Appointment</button>
-          )}
-          <button className="btn btn-lg qBtn rBtn d-flex flex-row-reverse">
-            {" "}
-            No Show
-          </button>
         </div>
       </div>
     );
