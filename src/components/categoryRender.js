cbdf1dddeb55a15585cae2f2ab594668d7e8eb8a
Nick Land <nicklandiam@gmail.com>
2020-04-20 19:33:38 -0600

    styled waitlisted

--- src/components/categoryRender.js
+++ src/components/categoryRender.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 import PouchDB from "pouchdb";
 
 // --- PROPS ---
-// currentQ: is the current Queue 
+// currentQ: is the current Queue
 // ETA: is how long the student will have to wait to see the Tutor
-// error: passes any errors that happens. 
+// error: passes any errors that happens.
 // --- PROPS ---
 
 const categoryRender = observer(
@@ -20,63 +20,7 @@ const categoryRender = observer(
       };
     }
 
-    /*queues:
-{
-  "_id": "newdate",
-  "_rev": "1-cd987fbcf59ed3ec6d748a7cc7c0e69c",
-  "studentID": 20155411,
-  "programID": "writing",
-  "appointmentStart": "getTime()",
-  "appointmentEnd": "getTime()"
-} */
-
-    /////////
-    ////////Moved this to cat store, will have to provide with student ID when called
-
-    // createQ = () => {
-    //   let uuid = new Date().getTime();
-    //   var db = new PouchDB(
-    //     "https://b705ce6d-2856-466b-b76e-7ebd39bf5225-bluemix.cloudant.com/programs"
-    //   );
-
-    //   let programID = this.props.name;
-    //   db.get(this.props.name)
-    //     .then(function(doc) {
-    //       //console.log(doc);
-    //       doc._rev = doc._rev;
-    //       let qObj = {
-    //         id: uuid,
-    //         studentID: 201554111,
-    //         programID: programID,
-    //         appointmentStart: 0,
-    //         appointmentEnd: 0
-    //       };
-    //       doc.activeQ.push(qObj);
-    //       return db.put(doc);
-    //     })
-    //     .then(function() {
-    //       db.get(programID);
-    //     })
-    //     .then(function(doc) {
-    //       console.log(doc);
-    //     });
-    //   // .then(function() {
-    //   //   // fetch again
-    //   //   return db.get(programID);
-    //   // })
-    //   // .catch(function(err) {
-    //   //   if (err.name === "conflict") {
-    //   //     db.get(programID).then(function(doc) {
-    //   //       doc.activeQ.push(qObj);
-    //   //     });
-    //   //   }
-    //   // });
-    // };
-
     handleWaitlist = () => {
-      //works off student ID now
-      //might have to add some methods to clear?
-      //unsure at this time
       let sID = sessionStorage.getItem("studentID");
       if (sID) {
         this.props.catStore.Waitlist(this.props.name, sID);
@@ -111,7 +55,6 @@ const categoryRender = observer(
           " minutes. \n Thank you for using Learner Success Services",
       };
 
-      //console.log(message);
       fetch("/SMS", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
